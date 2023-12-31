44a1dcd27ba800b2e84b69ca6fdee769cdf54aba
VagharshakGasparyan <vvagharshak@gmail.com>
2023-12-31 13:58:44 +0400

    message

---
+++ GVStrap/gvstrap.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    window.addEventListener('load', function () {
+        let sel = [".gv-dropdown-1"];
+        document.querySelectorAll(sel[0]).forEach(function (item) {
+            let clicked = false;
+            let last = item.querySelector(sel[0] + '>*:last-child');
+            // last.classList.toggle('disp-none');
+            item.addEventListener('click', function () {
+                last.classList.toggle('gv-disp-none');
+                clicked = true;
+            });
+            document.addEventListener('click', function () {
+                if (!clicked) last.classList.toggle('gv-disp-none', true);
+                clicked = false;
+            });
+        });
+    });
+})()
